845962443500986b84e5879fe0d1b51e405095a1
Utsav Chouhan <144726026+utsav0092@users.noreply.github.com>
2024-04-08 09:11:22 +0530

    master

--- PUP/views.py
+++ PUP/views.py
@@ -12,24 +12,26 @@ def profile(request):
     return render(request, 'profile.html')
 def login(request):
     return render(request, 'login.html')
-# def register(request):
-#     return render(request, 'register.html')
+
 def register(request):
     if request.method == 'POST':
+        # user_name = request.POST['user_name']
+        # user_password = request.POST['user_password']
+        # data = User.objects.create_user(user_name=user_name, user_password=user_password)
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('login')
     else:
         form = UserCreationForm()
-    return render(request,'register.html',{'form':form})
+    return render(request, 'register.html', {'form':form})
 
 def login(request):
     if request.method == "POST":
-        form = AuthenticationForm(request = request, data = request.POST)
+        form = AuthenticationForm(request = request, data= request.POST)
         if form.is_valid():
-            uname = form.cleaned_data['username']
-            upass = form.cleaned_data['password']
+            uname = form.cleaned_data['Username']
+            upass = form.cleaned_data['Password']
             user = authenticate(Username = uname, Password = upass)
             return render(request, 'profile.html')
     else:
